016d41b62bea0df073f8075083c011ea6f6b5ee0
sergevas <brighterr@gmail.com>
2024-07-13 10:31:47 +0300

    Add third Juggluco datasource & some improvements

--- src/main/java/dev/sergevas/tool/katya/gluco/bot/boundary/scheduler/SchedulerService.java
+++ src/main/java/dev/sergevas/tool/katya/gluco/bot/boundary/scheduler/SchedulerService.java
@@ -8,12 +8,14 @@ import dev.sergevas.tool.katya.gluco.bot.boundary.juggluco.JugglucoWebServerApiC
 import dev.sergevas.tool.katya.gluco.bot.boundary.telegram.KatyaGlucoBotApiClient;
 import dev.sergevas.tool.katya.gluco.bot.boundary.telegram.TelegramBotApiConfig;
 import dev.sergevas.tool.katya.gluco.bot.control.LastReadingCacheManager;
+import dev.sergevas.tool.katya.gluco.bot.entity.JugglucoStreamReading;
 import io.quarkus.logging.Log;
 import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-import java.util.Iterator;
+import java.util.Objects;
+import java.util.Optional;
 
 @ApplicationScoped
 public class SchedulerService {
@@ -42,29 +44,35 @@ public class SchedulerService {
 
     @Scheduled(every = "600s")
     public void updateReadings() {
-        tryToGetLastJugglucoStreamReading(jugglucoWebServerApiClient.getJugglucoWebServerApiList().iterator());
+        for (JugglucoWebServerApi jugglucoWebServerApi : jugglucoWebServerApiClient.getJugglucoWebServerApiList()) {
+            Optional<JugglucoStreamReading> jugglucoStreamReadingOpt = tryToGetLastJugglucoStreamReading(jugglucoWebServerApi);
+            if (jugglucoStreamReadingOpt.isPresent()) {
+                var jugglucoStreamReading = jugglucoStreamReadingOpt.get();
+                sendUpdate(jugglucoStreamReading.toFormattedString());
+                break;
+            }
+            Log.warn("Will try another URL if it is available");
+        }
     }
 
-    public void tryToGetLastJugglucoStreamReading(Iterator<JugglucoWebServerApi> jugglucoWebServerApiIterator) {
+    public Optional<JugglucoStreamReading> tryToGetLastJugglucoStreamReading(JugglucoWebServerApi jugglucoWebServerApi) {
+        Optional<JugglucoStreamReading> jugglucoStreamReadingOpt = Optional.empty();
         try {
-            while (jugglucoWebServerApiIterator.hasNext()) {
-                var jugglucoWebServerApi = jugglucoWebServerApiIterator.next();
-                var rawData = jugglucoWebServerApi.getStream();
-                var currentCsvStreamReading = csvDataReader.readToCsv(CsvStreamReading.class, rawData);
-                var currentReadings = ToJugglucoStreamReadingMapper.toJugglucoStreamReadingList(currentCsvStreamReading);
-                var lastReading = currentReadings.getLast();
-                Log.debugf("Have got the last reading: %s", lastReading);
-                if (lastReadingCacheManager.checkUpdatesJugglucoStreamReading(lastReading)) {
-                    var newReading = lastReadingCacheManager.getCachedJugglucoStreamReading().toFormattedString();
-                    Log.infof("********** Have got a new reading: %s", newReading);
-                    sendUpdate(newReading);
-                    break;
-                }
+            var rawData = jugglucoWebServerApi.getStream();
+            Objects.requireNonNull(rawData, "Raw Data must not be null!");
+            var currentCsvStreamReading = csvDataReader.readToCsv(CsvStreamReading.class, rawData);
+            var currentReadings = ToJugglucoStreamReadingMapper.toJugglucoStreamReadingList(currentCsvStreamReading);
+            var lastReading = currentReadings.getLast();
+            Log.debugf("Have got the last reading: %s", lastReading);
+            if (lastReadingCacheManager.checkUpdatesJugglucoStreamReading(lastReading)) {
+                var newReading = lastReadingCacheManager.getCachedJugglucoStreamReading().toFormattedString();
+                Log.infof("********** Have got a new reading: %s", newReading);
+                jugglucoStreamReadingOpt = Optional.of(lastReading);
             }
         } catch (Exception e) {
-            Log.error("Unable to fetch a new reading. Will try another URL if it is available", e);
-            tryToGetLastJugglucoStreamReading(jugglucoWebServerApiIterator);
+            Log.warn("Unable to fetch a new reading", e);
         }
+        return jugglucoStreamReadingOpt;
     }
 
     private void sendUpdate(final String text) {
