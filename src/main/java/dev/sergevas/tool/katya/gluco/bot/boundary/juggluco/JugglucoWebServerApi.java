7d6584303112065d62f89ada2843b134de75837f
sergevas <brighterr@gmail.com>
2025-04-17 16:06:44 +0300

    n progress

--- src/main/java/dev/sergevas/tool/katya/gluco/bot/boundary/juggluco/JugglucoWebServerApi.java
+++ src/main/java/dev/sergevas/tool/katya/gluco/bot/boundary/juggluco/JugglucoWebServerApi.java
@@ -3,15 +3,14 @@ package dev.sergevas.tool.katya.gluco.bot.boundary.juggluco;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.faulttolerance.Retry;
 
 //@RegisterRestClient(configKey = "juggluco-webserver-api")
 public interface JugglucoWebServerApi {
 
     @GET
-    @Path("/x/stream")
-    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("/query")
+    @Consumes("application/csv")
     @Retry(delay = 0, maxDuration = 11000, jitter = 500, maxRetries = 2)
     String getStream();
 }
--- src/main/java/dev/sergevas/tool/katya/gluco/bot/entity/ChangeStatus.java
+++ src/main/java/dev/sergevas/tool/katya/gluco/bot/entity/ChangeStatus.java
@@ -1,16 +1,31 @@
 package dev.sergevas.tool.katya.gluco.bot.entity;
 
+import java.util.Arrays;
+
 public enum ChangeStatus {
 
-    STABLE("→"),
-    FALLING_QUICKLY("↓"),
-    RISING_QUICKLY("↑"),
-    RISING("↗"),
-    FALLING("↘");
+    FLAT("Flat", "→"),
+    SINGLE_DOWN("SingleDown", "↓"),
+    DOUBLE_DOWN("DoubleDown", "↓"),
+    SINGLE_UP("SingleUp", "↑"),
+    DOUBLE_UP("DoubleUp", "↑"),
+    FORTY_FIVE_UP("FortyFiveUp", "↗"),
+    FORTY_FIVE_DOWN("FortyFiveDown", "↘"),
+    UNDEFINED(null, "-");
 
+    private final String name;
     private final String mark;
-    ChangeStatus(String mark) {
+
+    ChangeStatus(String name, String mark) {
+        this.name = name;
         this.mark = mark;
+    }
+
+    public static ChangeStatus fromName(String name) {
+        return Arrays.stream(ChangeStatus.values())
+                .filter(status -> status.name.equals(name))
+                .findFirst()
+                .orElse(UNDEFINED);
     }
 
     public String getMark() {
