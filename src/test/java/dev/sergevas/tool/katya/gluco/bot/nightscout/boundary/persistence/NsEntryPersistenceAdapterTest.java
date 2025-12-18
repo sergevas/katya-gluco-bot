4f10d5dc6fc64041cdda3b23b8230b672d120661
sergevas <brighterr@gmail.com>
2025-12-18 20:39:49 +0300

    App events publish feature implementation is in progress

--- src/test/java/dev/sergevas/tool/katya/gluco/bot/nightscout/boundary/persistence/NsEntryPersistenceAdapterTest.java
+++ src/test/java/dev/sergevas/tool/katya/gluco/bot/nightscout/boundary/persistence/NsEntryPersistenceAdapterTest.java
@@ -1,11 +1,16 @@
 package dev.sergevas.tool.katya.gluco.bot.nightscout.boundary.persistence;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
+@DataJpaTest
+@ActiveProfiles("test")
 class NsEntryPersistenceAdapterTest {
 
     @Test
-    void storeNsEntries() {
+    void givenNewNsEntries_whenStore_thenShouldStoreAndPublishAppEventForEachStoredNsEntry() {
+
     }
 }
