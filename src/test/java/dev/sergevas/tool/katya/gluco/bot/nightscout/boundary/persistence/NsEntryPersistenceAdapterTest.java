e921ad7fe80a43466f31f40144ef6ccfd2780416
sergevas <brighterr@gmail.com>
2025-12-18 16:30:02 +0300

    In progree

---
+++ src/test/java/dev/sergevas/tool/katya/gluco/bot/nightscout/boundary/persistence/NsEntryPersistenceAdapterTest.java
@@ -0,0 +1,11 @@
+package dev.sergevas.tool.katya.gluco.bot.nightscout.boundary.persistence;
+
+import org.junit.jupiter.api.Test;
+
+
+class NsEntryPersistenceAdapterTest {
+
+    @Test
+    void storeNsEntries() {
+    }
+}
