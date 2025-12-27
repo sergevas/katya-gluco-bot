534d6501bd1acfba18960a6b6ca3385cd9a0957e
sergevas <brighterr@gmail.com>
2025-12-27 18:19:20 +0300

    Cleanups

--- src/main/java/dev/sergevas/tool/katya/gluco/bot/nightscout/boundary/appevent/NsEntryPublishedLoggingHandler.java
+++ src/main/java/dev/sergevas/tool/katya/gluco/bot/nightscout/boundary/appevent/NsEntryPublishedLoggingHandler.java
@@ -4,7 +4,6 @@ import dev.sergevas.tool.katya.gluco.bot.nightscout.control.NsEntryPublishedUseC
 import dev.sergevas.tool.katya.gluco.bot.nightscout.entity.NsEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.event.EventListener;
 
 public class NsEntryPublishedLoggingHandler implements NsEntryPublishedUseCase {
 
@@ -12,12 +11,7 @@ public class NsEntryPublishedLoggingHandler implements NsEntryPublishedUseCase {
 
     @Override
     public void handle(NsEntry nsEntry) {
-        LOG.debug("Enter {}.handle() nsEntry = {}", NsEntryPublishedLoggingHandler.class, nsEntry);
-        LOG.debug("Exit {}.handle()", NsEntryPublishedLoggingHandler.class);
-    }
-
-    @EventListener
-    public void onNsEntryPublished(NsEntry nsEntry) {
-        this.handle(nsEntry);
+        LOG.info("Enter {}.handle() nsEntry = {}", NsEntryPublishedLoggingHandler.class, nsEntry);
+        LOG.info("Exit {}.handle()", NsEntryPublishedLoggingHandler.class);
     }
 }
